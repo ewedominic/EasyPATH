4dd7732b0309e23ff90984b5bf35ff29028dddb8
harmonmi <harmon.michael@outlook.com>
2015-12-27 22:55:14 -0500

    Added Installer and logic barebones.
    Created an MSI installer to place EasyPATH.exe on the user's computer.
    Also added the basic logic and comments to the other CS files.

--- EasyPATH/EasyPATH.cs
+++ EasyPATH/EasyPATH.cs
@@ -1,16 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace EasyPATH
 {
-    class EasyPATH
+    /// <summary>
+    /// Handles the control of the EasyPATH application.
+    /// </summary>
+    public class EasyPATH
     {
+        /// <summary>
+        /// Determines which registry operation to run based on the input arguments.
+        /// </summary>
+        /// <param name="args">Input arguments. This should either be "install" or "uninstall" based on whether or not the user has previously installed EasyPATH.</param>
         static void Main(string[] args)
         {
+            // Make sure there is only 1 argument.
+            if (args.Length != 1)
+                return;
 
+            // Get the location of EasyPATH.exe.
+            string installPath = Path.GetFullPath(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+            // Make sure EasyPATH.exe actually exists.
+            if (!File.Exists(installPath))
+                return;
+
+            // Create an instance of RegistryHandler.
+            RegistryHandler regHandler = new RegistryHandler();
+
+            // If the input argument is "uninstall", remove the custom registry entry from the computer, otherwise if the input argument is "install" add a custom registry to the computer.
+            if (args[0] == "uninstall")
+            {
+                regHandler.removeRegistryKey();
+            }
+            else if (args[0] == "install")
+            {
+                regHandler.addRegistryKey(installPath);
+            }
         }
     }
 }
---
+++ EasyPATH/RegistryHandler.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace EasyPATH
+{
+    /// <summary>
+    /// Handles the registry editing for the EasyPATH application.
+    /// </summary>
+    public class RegistryHandler
+    {
+        // The root sub key of the new registry entry.
+        private string root = @"*\shell\EasyPATH";
+        // The command sub key of the root registry entry.
+        private string command = @"*\shell\EasyPATH\command";
+
+        /// <summary>
+        /// Adds a custom registry key to introduce a new context menu option upon right clicking.
+        /// </summary>
+        /// <param name="installPath">The path to EasyPATH.exe.</param>
+        /// <returns></returns>
+        public bool addRegistryKey(string installPath)
+        {
+
+            return false;
+        }
+
+        /// <summary>
+        /// Removes the custom registry key.
+        /// </summary>
+        /// <returns></returns>
+        public bool removeRegistryKey()
+        {
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks whether or not the custom registry key already exists.
+        /// </summary>
+        /// <returns></returns>
+        private bool keyExists()
+        {
+
+            return false;
+        }
+    }
+}
